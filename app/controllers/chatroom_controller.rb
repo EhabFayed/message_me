5f1e42e6460476985e3549b1b0fc99cf9525d9e6
EhabFayed <ehabahmed2233@gmail.com>
2024-02-17 02:48:45 +0200

    form for auth done

--- app/controllers/chatroom_controller.rb
+++ app/controllers/chatroom_controller.rb
@@ -1,9 +1,5 @@
 class ChatroomController < ApplicationController
     def index
-        respond_to do |format|
-            format.html
-        end
-        # byebug
+       @messages=Message.all
     end
-
 end
