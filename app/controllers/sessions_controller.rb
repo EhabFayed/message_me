ca4ff41bead300807156c22a31d2112c27b43494
EhabFayed <ehabahmed2233@gmail.com>
2024-02-19 21:39:20 +0200

    flash messages done and login function also

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,4 +2,23 @@ class SessionsController < ApplicationController
     def new 
         
     end
+    def create
+        user=User.find_by(username: params[:session][:username])
+        if user && user.authenticate( params[:session][:password])
+        session[:user_id]=user.id
+        flash[:success]="uhave successfully logged it "
+        redirect_to root_path
+
+        else
+           flash.now[:error]="user name or password is incorrenct"
+           render 'new'  
+        end
+    end
+
+    def destroy
+        session[:user_id]=nil
+        flash[:success]="u have successfully logged out"
+        redirect_to login_path
+
+    end
 end
